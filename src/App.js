cf15d3b0bcc3f2b11c544718a6114ec9d0b95fc1
Arif Shariati <arif.shariati@kns.com.tr>
2021-06-11 19:10:44 +0300

    finalized card list component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import myList from './myList';
 
 function App() {
+
+  const [personList, updatePersonList] = useState(myList);
+
+  const handleOnDragEnd = (result) => {
+    
+    // if destination is not droppable area, return so it is dragged back to its original position
+    if(!result.destination) return;
+
+    // make new array from personList 
+    const items = Array.from(personList);
+
+    // splice items array from the source of the object moved
+    const [reorderedItem] = items.splice(result.source.index, 1);
+
+    // re-position the sliced array part back to the items array
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    // update our state with re-ordered array (items)
+    updatePersonList(items);
+
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>People List</h1>
+        {/* re-order our stat once object is dragged to new position (result contains source and destination indexes) */}
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="personsList">
+            {
+              (provided) => (
+                <ul className="personsList" {...provided.droppableProps} ref={provided.innerRef}>
+                  {
+                    personList.map(({id, name, pic}, index) => {
+                      return (
+                        <Draggable key={id} draggableId={id} index={index}>
+                          {
+                            (provided) => (
+                              <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                <div className="personPic">
+                                  <img src={pic} alt={name} />
+                                </div>
+                                <p> {name} </p> 
+                              </li>
+                            )
+                          }
+                        </Draggable>
+                      )
+                    })
+                  }
+                  {provided.placeholder}
+                </ul>
+              )
+            }
+          </Droppable>
+        </DragDropContext>
       </header>
+      <p>developed in React Js - Arif Shariati</p>
     </div>
   );
 }
---
+++ src/myList.js
@@ -0,0 +1,31 @@
+import {v4 as uuid} from 'uuid';
+
+const myList = [
+    {
+        id:uuid(),
+        name:"Caroline",
+        pic:"/images/image1.jpeg"
+    },
+    {
+        id:uuid(),
+        name:"Artist",
+        pic:"/images/image2.png"
+    },
+    {
+        id:uuid(),
+        name:"Aisha",
+        pic:"/images/image3.jpeg"
+    },
+    {
+        id:uuid(),
+        name:"Somya",
+        pic:"/images/image4.jpeg"
+    },
+        {
+        id:uuid(),
+        name:"Mike",
+        pic:"/images/image5.jpeg"
+    }
+];
+
+export default myList;
